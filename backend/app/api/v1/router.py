b103b3781801876c630d583117a097380d2a106e
Sameer <sam2164874@gmail.com>
2026-03-22 20:31:21 +0530

    fix

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from ...endpoints import photo
+from .endpoints import photo
 
 api_router = APIRouter()
 api_router.include_router(photo.router, prefix="/photo", tags=["photo"])
